35ecdc3989ce075463eb60268ccde48864fa8dfa
katiebrnr <katiefarquhar@katies-air.domain>
2016-02-20 09:53:12 -0800

    Initial Commit

---
+++ CrossfitTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrossfitTracker
+//
+//  Created by Katie Farquhar on 2/20/16.
+//  Copyright © 2016 Katie Berner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrossfitTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrossfitTracker
+//
+//  Created by Katie Farquhar on 2/20/16.
+//  Copyright © 2016 Katie Berner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
