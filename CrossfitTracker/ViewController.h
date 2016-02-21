dfb78d6bb3092f167a62b54fb52a689b2ed7e083
katiebrnr <katiefarquhar@Katies-Air.domain>
2016-02-20 23:26:06 -0800

    commit of prototype development code for Crossfit tracking app

--- CrossfitTracker/ViewController.h
+++ CrossfitTracker/ViewController.h
@@ -10,6 +10,33 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UISegmentedControl *wodType;
+
+@property (weak, nonatomic) IBOutlet UITextView *wodDescription;
+
+@property (weak, nonatomic) IBOutlet UIButton *saveButton;
+
+@property (weak, nonatomic) IBOutlet UILabel *introLabel;
+
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+
+@property (weak, nonatomic) IBOutlet UITextField *timeTextBox;
+
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *amrapLabel;
+
+@property (weak, nonatomic) IBOutlet UITextField *amrapTextBox;
+
+@property (weak, nonatomic) IBOutlet UILabel *roundsLabel;
+
+@property (weak, nonatomic) IBOutlet UITextField *roundsTextBox;
+
+@property (weak, nonatomic) IBOutlet UITextField *repsTextBox;
+
+@property (weak, nonatomic) IBOutlet UILabel *plusLabel;
+@property (weak, nonatomic) IBOutlet UILabel *wodDescriptionLabel;
+
 
 @end
 
--- CrossfitTracker/ViewController.m
+++ CrossfitTracker/ViewController.m
@@ -7,16 +7,140 @@
 //
 
 #import "ViewController.h"
+#import <EventKitUI/EventKitUI.h>
 
 @interface ViewController ()
+// The database with calendar events and reminders
+@property (strong, nonatomic) EKEventStore *eventStore;
+
+// Indicates whether app has access to event store
+//@property (nonatomic) BOOL isAccessToEventStoreGranted;
 
 @end
 
 @implementation ViewController
 
+@synthesize wodType;
+@synthesize datePicker;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view
+    self.introLabel.text = @"Track your Crossfit workouts here! You've got this!";
+    self.wodDescriptionLabel.text = @"Workout description (including weights, modifications, etc):";
+    
+    [wodType addTarget:self action:@selector(segmentAction:) forControlEvents: UIControlEventValueChanged];
+    wodType.selectedSegmentIndex = 1; // Default to For Time
+    [self.view addSubview:wodType];
+    
+    self.amrapLabel.hidden = YES;
+    self.amrapTextBox.hidden = YES;
+    self.roundsLabel.hidden = YES;
+    self.roundsTextBox.hidden = YES;
+    self.repsTextBox.hidden = YES;
+    self.plusLabel.hidden = YES;
+}
+
+- (void)segmentAction:(UISegmentedControl *)segment
+{
+    switch (segment.selectedSegmentIndex) {
+        case 0: {
+            
+            //self.view.backgroundColor = [UIColor blueColor]; //for testing purposes
+            self.timeLabel.hidden = YES;
+            self.timeTextBox.hidden = YES;
+            
+            self.amrapLabel.text = @"AMRAP";
+            self.amrapLabel.hidden = NO;
+            self.amrapTextBox.hidden = NO;
+            self.roundsLabel.text = @"Rounds + Reps: ";
+            self.roundsLabel.hidden = NO;
+            self.roundsTextBox.hidden = NO;
+            self.repsTextBox.hidden = NO;
+            self.plusLabel.text = @"+";
+            self.plusLabel.hidden = NO;
+            break;
+            
+        }
+        case 1: {
+            
+            //self.view.backgroundColor = [UIColor greenColor]; //for testing purposes
+            
+            self.timeLabel.text = @"Final Time: ";
+            self.timeLabel.hidden = NO;
+            self.timeTextBox.hidden = NO;
+            
+            self.amrapLabel.hidden = YES;
+            self.amrapTextBox.hidden = YES;
+            self.roundsLabel.hidden = YES;
+            self.roundsTextBox.hidden = YES;
+            self.repsTextBox.hidden = YES;
+            self.plusLabel.hidden = YES;
+            
+            break;
+            
+        }
+        case 2: {
+            //self.view.backgroundColor = [UIColor yellowColor]; //for testing purposes
+            
+            self.timeLabel.hidden = YES;
+            self.timeTextBox.hidden = YES;
+            
+            self.amrapLabel.hidden = YES;
+            self.amrapTextBox.hidden = YES;
+            self.roundsLabel.hidden = YES;
+            self.roundsTextBox.hidden = YES;
+            self.repsTextBox.hidden = YES;
+            self.plusLabel.hidden = YES;
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+- (IBAction)clickSaveButton:(id)sender {
+    //self.view.backgroundColor = [UIColor redColor]; //for testing purposes
+    
+    //Request access to events
+    EKEventStore *store = [EKEventStore new];
+    [store requestAccessToEntityType:EKEntityTypeEvent completion:^(BOOL granted, NSError *error) {
+        if (!granted) { return; }
+        EKEvent *event = [EKEvent eventWithEventStore:store];
+        
+        event.title = @"Crossfit!";
+        
+        if ([self.timeTextBox.text isEqual:@""] == FALSE)
+        {
+            event.notes = [NSString stringWithFormat:@"Final Time: %@ \n%@",
+             self.timeTextBox.text, self.wodDescription.text];
+        }
+        else if ([self.amrapTextBox.text isEqual:@""] == FALSE)
+        {
+            event.notes = [NSString stringWithFormat:@"AMRAP %@ \n%@ \n%@ %@ %@ %@",
+             self.amrapTextBox.text, self.wodDescription.text, self.roundsLabel.text, self.roundsTextBox.text, self.plusLabel.text, self.repsTextBox.text];
+        }
+        else
+        {
+            event.notes = self.wodDescription.text;
+        }
+        
+        event.startDate = [datePicker date];
+        event.endDate = [event.startDate dateByAddingTimeInterval:60*60];  //set 1 hour event
+        
+        event.calendar = [store defaultCalendarForNewEvents];
+        NSError *err = nil;
+        [store saveEvent:event span:EKSpanThisEvent commit:YES error:&err];
+        
+        [self createAlert];
+    }];
+}
+
+- (void)createAlert {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Success!" message:@"Your workout has been saved to the calendar." delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
+                                  [alertView show];
+                                  });
 }
 
 - (void)didReceiveMemoryWarning {
